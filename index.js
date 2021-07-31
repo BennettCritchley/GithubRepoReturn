07fc852d5dba02f1e7f3e92c55fdd9e5e1d86b51
BennettCritchley <Bennett.critchley1@gmail.com>
2021-07-31 12:57:20 -0600

    displaying info

--- index.js
+++ index.js
@@ -15,7 +15,23 @@ function getRepos(searchTerm, maxResults=10) {
   
   fetch(url)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(responseJson => {
+      console.log(responseJson)
+      displayRepos(responseJson)});
+
+}
+
+function displayRepos(repos) {
+  $('#results-list').empty()
+  repos.forEach(repo => {
+    const name = repo.name;
+    const repoUrl = repo['clone_url'];
+    const repoTextName = `<h3>${name}</h3>`
+    const repoTextUrl = `<a href="${repoUrl}">${repoUrl}</a>`
+    const listElement = $('<li></li>')
+    listElement.append(repoTextName, repoTextUrl)
+    $('#results-list').append(listElement);
+  });
 }
 
 $(watchForm)
