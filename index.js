5e427f5fefe32e1646adce341606e0604554c9ec
BennettCritchley <Bennett.critchley1@gmail.com>
2021-08-02 18:55:30 -0600

    editing code

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-
-
 function watchForm() {
-  $('form').submit(event => {
+  $("form").submit((event) => {
     event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
-    const maxResults = $('#js-max-results').val();
-    getRepos(searchTerm, maxResults)
-    console.log(searchTerm)
+    const searchTerm = $("#js-search-term").val();
+    const maxResults = $("#js-max-results").val();
+    getRepos(searchTerm, maxResults);
+    console.log(searchTerm);
   });
 }
 
-function getRepos(searchTerm, maxResults=10) {
-  const url = `https://api.github.com/users/${searchTerm}/repos`
-  
-  fetch(url)
-    .then(response => response.json())
-    .then(responseJson => {
-      console.log(responseJson)
-      displayRepos(responseJson)});
+function getRepos(searchTerm, maxResults = 10) {
+  const url = `https://api.github.com/users/${searchTerm}/repos`;
 
+  fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+      displayRepos(responseJson);
+    });
 }
 
 function displayRepos(repos) {
-  $('#results-list').empty()
-  repos.forEach(repo => {
+  $("#results-Section").removeClass("hidden");
+  $("#results-list").empty();
+  repos.forEach((repo) => {
     const name = repo.name;
-    const repoUrl = repo['clone_url'];
-    const repoTextName = `<h3>${name}</h3>`
-    const repoTextUrl = `<a href="${repoUrl}">${repoUrl}</a>`
-    const listElement = $('<li></li>')
-    listElement.append(repoTextName, repoTextUrl)
-    $('#results-list').append(listElement);
+    const repoUrl = repo["clone_url"];
+    const repoTextName = `<h3>${name}</h3>`;
+    const repoTextUrl = `<a href="${repoUrl}">${repoUrl}</a>`;
+    const listElement = $("<li></li>");
+    listElement.append(repoTextName, repoTextUrl);
+    $("#results-list").append(listElement);
   });
 }
 
-$(watchForm)
+$(watchForm);
